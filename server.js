49b217d83549ce79fd642ad3bd06680f8dba7cc2
pepito5962 <46892896+pepito5962@users.noreply.github.com>
2021-04-24 18:00:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const router = jsonServer.router(
   "https://medical-b7967-default-rtdb.firebaseio.com/"
 );
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-console.log(`Express is working on port ${port}`);
+const port = process.env.PORT || 3000 || 4200 || 5000;
 
 server.use(middlewares);
 server.use(router);
