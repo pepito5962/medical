0c1276d45db091b1fb2c50f23903ac0534119400
pepito5962 <46892896+pepito5962@users.noreply.github.com>
2021-04-24 17:47:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // server.js
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(
+  "https://medical-b7967-default-rtdb.firebaseio.com/"
+);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
